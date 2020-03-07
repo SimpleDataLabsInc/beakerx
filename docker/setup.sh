d23022f03cb09b241628800c29e9dae42c893cf5
Maciej Szpakowski <several27@icloud.com>
2020-03-07 15:52:36 -0800

    removing failing command

--- docker/setup.sh
+++ docker/setup.sh
@@ -19,7 +19,7 @@ source activate beakerx
 (cd beakerx; pip install -r requirements.txt --verbose)
 beakerx install
 beakerx_databrowser install
-jupyter labextension install @jupyter-widgets/jupyterlab-manager
+#jupyter labextension install @jupyter-widgets/jupyterlab-manager
 (cd js/lab; jupyter labextension install .)
 
 rm -rf docker .DS_Store .git .gradle .idea jitpack.yml kernel RELEASE.md test .cache .yarn .local logs .ipynb_checkpoints
